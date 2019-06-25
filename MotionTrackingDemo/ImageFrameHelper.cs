d19f1eb14f4de21b9ceb439ac2b20c889439ce37
Werlin <wxh2fine@gmail.com>
2019-06-25 19:27:00 +0800

    Basic function fulfilled

---
+++ MotionTrackingDemo/ImageFrameHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MotionTrackingDemo
+{
+    public class SingleImageFrame
+    {
+
+        public ImageSource FrameImageSource { get; private set; }
+
+        public SingleImageFrame(ImageSource image)
+        {
+            FrameImageSource = image;
+        }
+    }
+
+    public class FrameImageCache : List<SingleImageFrame>
+    {
+        public void AddImage(ImageSource image)
+        {
+            Add(new SingleImageFrame(image));
+        }
+    }
+}
--- MotionTrackingDemo/MainWindow.xaml.cs
+++ MotionTrackingDemo/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Accord.Video;
+using Accord.Video.DirectShow;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +23,56 @@ namespace MotionTrackingDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private OpenFileDialog openFileDialog = new OpenFileDialog();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // create video source
+                FileVideoSource fileSource = new FileVideoSource(openFileDialog.FileName);
+
+                // open it
+                OpenVideoSource(fileSource);
+            }
+            
+        }
+
+        private void OpenVideoSource(IVideoSource source)
+        {
+            
+            // stop current video source
+            CloseCurrentVideoSource();
+
+            // start new video source
+            framePlayer.VideoSource = source;
+            framePlayer.Start();
+        }
+
+        private void CloseCurrentVideoSource()
+        {
+            if (framePlayer.VideoSource != null)
+            {
+                framePlayer.SignalToStop();
+
+                // wait ~ 3 seconds
+                for (int i = 0; i < 30; i++)
+                {
+                    if (!framePlayer.IsRunning)
+                        break;
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                if (framePlayer.IsRunning)
+                {
+                    framePlayer.Stop();
+                }
+
+                framePlayer.VideoSource = null;
+            }
         }
     }
 }
